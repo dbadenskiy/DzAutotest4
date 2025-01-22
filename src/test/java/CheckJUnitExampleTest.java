6927a608d6ad10f6161478a884fda53fe3074ee7
Dmitriy Badenskiy <dima.badenskiy96@mail.ru>
2025-01-22 14:39:50 +0300

    Intermediate test

--- src/test/java/CheckJUnitExampleTest.java
+++ src/test/java/CheckJUnitExampleTest.java
@@ -12,10 +12,17 @@ public class CheckJUnitExampleTest {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1474x822";
         Configuration.timeout = 5000;
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void checkJUniteCodeExampleTest() {
         open("https://github.com/");
+        $(".search-input").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        $("a[href='/selenide/selenide']").click();
+        $("#wiki-tab").click();
+
+
     }
 }
